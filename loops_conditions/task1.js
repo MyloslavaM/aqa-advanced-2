c0606086ee4e53db974633479982036cd9696874
MyloslavaM <myloslava.martynova@gmail.com>
2023-10-14 13:22:19 +0200

    HW2 pull fromUI

---
+++ loops_conditions/task1.js
@@ -0,0 +1,14 @@
+// Завдання 1: Визначення рівня успішності студента
+const averageGrade = 88
+
+if(averageGrade < 60) {
+    console.log('Незадовільно')
+}else if(averageGrade <=70){
+    console.log(' "Задовільно"')
+}else if(averageGrade <=80){
+    console.log(' "Добре"')
+}else if(averageGrade <=90){
+    console.log('"Дуже добре"')
+}else{
+    console.log("Відмінно")
+}
